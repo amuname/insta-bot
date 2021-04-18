642d9b9dad7cfdcf7ac8f0371c697f3f737783c3
amuname <74045707+amuname@users.noreply.github.com>
2021-04-18 13:29:40 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,22 @@
 const pptr = require('puppeteer');
 
 (async ()=>{
-	const browser = await pptr.launch(/*{headless:false,}*/);
+	const browser = await pptr.launch({headless:false,});
 	const page = await browser.newPage();
 
 	const test = await page.evaluate(
-		{a:function(){
-			this.a = true;
+		()=>{
+			const test= function (){
+		       	console.log(this)
+		       	this.test = true
+		       	return true
+		    };
+		   	return  test()
 
 		}
-	})
+	)
 
 	console.log(test);
-	console.log(test.a);
+	// console.log(test.a);
 
 })()

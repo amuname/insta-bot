5f26e46a8f5d62a5e3610ac31d3f142d09c718f8
alex mesh <3steps2die@gmail.com>
2021-04-22 17:02:56 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,15 @@
 const pptr = require('puppeteer');
+const fs = require('fs');
 
 (async ()=>{
 	const browser = await pptr.launch({headless:false,});
 	const page = await browser.newPage();
-
-	const test = await page.evaluate(
-		()=>{
-			const test= function (){
-		       	console.log(this)
-		       	this.test = true
-		       	return true
-		    };
-		   	return  test()
-
-		}
-	)
-
-	console.log(test);
+	const browserWSEndpoint = browser.wsEndpoint();
+	await fs.appendFile('message.txt',browserWSEndpoint,(err)=>{
+		if (err) throw err
+	})
+	browser.disconnect()
+	// console.log(test);
 	// console.log(test.a);
 
 })()
